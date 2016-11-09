7902e213af3c8064befdea5ebd82a72d34c9cfd7
Brian Mathiyakom <brian@rarevisions.net>
2016-11-08 16:54:38 -0800

    Add content-length and body debug statements
    So that we know whether our embedded client is sending all the data it
    has and whether that data is of the correct length.

--- app.rb
+++ app.rb
@@ -4,6 +4,14 @@ require 'json'
 post '/temperature' do
   halt 401 unless authenticated?(params)
 
+  if params['f'] == nil || params['f'].empty?
+    halt 400
+  end
+
+  if params['ts'] == nil || params['ts'].empty?
+    halt 400
+  end
+
   temp_in_f = params['f']
   timestamp = params['ts']
   result = {
@@ -12,7 +20,12 @@ post '/temperature' do
     timestamp: timestamp
   }
 
+  request.body.rewind
+  body = request.body.read
+  puts "DEBUG Content-Length: #{request.content_length} - RequestBody: #{body}"
+
   puts JSON.generate(result)
+
 end
 
 def authenticated?(params)
